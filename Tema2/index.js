e7c9476d0efbcba5ac48f38e3e7abdf25659925a
cristibctr <cristian.bucataru@gmail.com>
2022-03-10 16:46:05 +0200

    Implemented PUT method

--- Tema2/index.js
+++ Tema2/index.js
@@ -11,90 +11,238 @@ const server = http.createServer((req, res) => {
     console.log('request ', req.url);
     var urlObj = url.parse(req.url, true);
     var queryObject = urlObj.query;
+    appNr = req.url.split('/')[3]
     if(urlObj.pathname === "/api/apps")
     {
-        if(req.method === "GET")
-        {
-            var apps = []
-            page = queryObject.page == null ? undefined : queryObject.page * 5
-            query = queryObject.page == null ? 'SELECT id, name, company, downloads FROM applications' : `SELECT id, name, company, downloads FROM applications limit 5 offset ?`
-            db.serialize(()=>{
-                db.each(query, [page] , (err, row) => {
-                    if(err){
-                        console.log(err.message)
-                    }
-                    apps.push(row)
-                }, () => {
-                    res.writeHead(200, {'Content-Type': 'application/json'});
-                    res.end(JSON.stringify(apps))
-                })
-            })
-        }
-        else if(req.method === "POST")
-        {
-            //Create user and return user profile link
-            var data = ''
-            req.on('data', (chunk) => {
-                data += chunk
-            })
-            req.on('end', () => {
-                app = JSON.parse(data)
-                if (typeof app === 'object' && !Array.isArray(app) && app !== null && app.name !== undefined && app.version !== undefined && app.company !== undefined)
-                {
-                    query = `INSERT INTO applications (name, version, company
-                        ${"app_hash" in app ? ', app_hash' : ''}
-                        ${"includes_ads" in app ? ', includes_ads' : ''}
-                        ${"downloads" in app ? ', downloads' : ''}) VALUES (?, ?, ?
-                        ${"app_hash" in app ? ', ?' : ''}
-                        ${"includes_ads" in app ? ', ?' : ''}
-                        ${"downloads" in app ? ', ?' : ''})`
-                    db.run(query, [app.name, app.version, app.company, app.app_hash, app.includes_ads, app.downloads], function(err) {
+        switch(req.method){
+            case "GET":
+                var apps = []
+                page = queryObject.page == null ? undefined : queryObject.page * 5
+                query = queryObject.page == null ? 'SELECT id, name, company, downloads FROM applications' : `SELECT id, name, company, downloads FROM applications limit 5 offset ?`
+                db.serialize(()=>{
+                    db.each(query, [page] , (err, row) => {
                         if(err){
-                            if(err.message.includes('UNIQUE'))
-                            {
-                                res.writeHead(409, {'Content-Type': 'application/json'});
-                                res.end(`{"field" : "name", "message" : "duplicate"}`)
+                            console.log(err.message)
+                        }
+                        apps.push(row)
+                    }, () => {
+                        res.writeHead(200, {'Content-Type': 'application/json'});
+                        res.end(JSON.stringify(apps))
+                    })
+                })
+                break
+            case "POST":
+                //Create user and return user profile link
+                var data = ''
+                req.on('data', (chunk) => {
+                    data += chunk
+                })
+                req.on('end', () => {
+                    app = JSON.parse(data)
+                    if (typeof app === 'object' && !Array.isArray(app) && app !== null && app.name !== undefined && app.version !== undefined && app.company !== undefined)
+                    {
+                        query = `INSERT INTO applications (name, version, company
+                            ${"app_hash" in app ? ', app_hash' : ''}
+                            ${"includes_ads" in app ? ', includes_ads' : ''}
+                            ${"downloads" in app ? ', downloads' : ''}) VALUES (?, ?, ?
+                            ${"app_hash" in app ? ', ?' : ''}
+                            ${"includes_ads" in app ? ', ?' : ''}
+                            ${"downloads" in app ? ', ?' : ''})`
+                        var queryArr = [app.name, app.version, app.company, app.app_hash, app.includes_ads, app.downloads]
+                        queryArr = queryArr.filter(function( element ) {
+                                return element !== undefined;
+                                });
+                        db.run(query, queryArr, function(err) {
+                            if(err){
+                                if(err.message.includes('UNIQUE'))
+                                {
+                                    res.writeHead(409, {'Content-Type': 'application/json'});
+                                    res.end(`{"field" : "name", "message" : "duplicate"}`)
+                                }
+                                else{
+                                    console.log(err.message)
+                                    res.statusCode = 500
+                                    res.end()
+                                }
                             }
                             else{
+                                //res.statusCode = 201
+                                res.writeHead(201, {'Location': `/api/apps/${this.lastID}`})
+                                res.end()
+                            }
+                        })
+
+                    }
+                    else{
+                        res.statusCode = 400
+                        res.end()
+                    }
+                })
+                break;
+            case "PUT":
+                var data = ''
+                req.on('data', (chunk) => {
+                    data += chunk
+                })
+                req.on('end', () => {
+                    apps = JSON.parse(data)
+                    if(!Array.isArray(apps))
+                    {
+                        res.writeHead(400, {'Content-Type': 'text/html'});
+                        res.end(`wrong format`)
+                        return
+                    }
+                    for(var app of apps){
+                        if(app.name == undefined || app.version == undefined || app.company == undefined)
+                        {
+                            console.log(app)
+                            res.writeHead(400, {'Content-Type': 'text/html'});
+                            res.end(`wrong format`)
+                            return
+                        }
+                    }
+                    db.serialize(() => {
+                        db.run('DELETE FROM applications', (err) => {
+                            if(err){
                                 console.log(err.message)
                                 res.statusCode = 500
                                 res.end()
+                                return
                             }
+                        })
+                        for(var app of apps){
+                            var query = `INSERT INTO applications (name, version, company
+                                ${"app_hash" in app ? ', app_hash' : ''}
+                                ${"includes_ads" in app ? ', includes_ads' : ''}
+                                ${"downloads" in app ? ', downloads' : ''}) VALUES (?, ?, ?
+                                ${"app_hash" in app ? ', ?' : ''}
+                                ${"includes_ads" in app ? ', ?' : ''}
+                                ${"downloads" in app ? ', ?' : ''})`
+                            var queryArr = [app.name, app.version, app.company, app.app_hash, app.includes_ads, app.downloads]
+                            queryArr = queryArr.filter(function( element ) {
+                                    return element !== undefined;
+                                 });
+                            db.run(query, queryArr, (err) => {
+                                if(err){
+                                    console.log(err.message)
+                                    res.statusCode = 500
+                                    res.end()
+                                    return
+                                }
+                            })
                         }
-                        else{
-                            //res.statusCode = 201
-                            res.writeHead(201, {'Location': `/api/apps/${this.lastID}`})
-                            res.end()
-                        }
+                        res.statusCode = 200
+                        res.end()
                     })
+                    // db.get('SELECT * FROM applications WHERE id = ?', [element.id], (err, row) => {
 
-                }
-                else{
-                    res.statusCode = 400
-                    res.end()
-                }
-            })
-        }
-        else{
-            res.writeHead(405, {'Content-Type': 'text/html'});
-            res.end("Method not allowed")
+                    // })
+                })
+                break
+            default:
+                res.writeHead(405, {'Content-Type': 'text/html'})
+                res.end("Method not allowed")
         }
     }
-
     // /api/apps/:id
-    appNr = req.url.split('/')[3]
-    if(urlObj.pathname.includes("/api/apps") && !isNaN(appNr) )
+    else if(urlObj.pathname.includes("/api/apps") && !isNaN(appNr) )
     {
-        if(req.method === "GET")
+        switch(req.method)
         {
-            db.get('SELECT * FROM applications WHERE id = ?', [appNr], (err, row) => {
-                if(err){
-                    console.log(err.message)
-                }
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.end(JSON.stringify(row))
-            })
+            case "GET":
+                db.get('SELECT * FROM applications WHERE id = ?', [appNr], (err, row) => {
+                    if(err){
+                        console.log(err.message)
+                        res.statusCode = 500
+                        res.end()
+                        return
+                    }
+                    if(row == undefined)
+                    {
+                        res.statusCode = 404
+                        res.end()
+                        return
+                    }
+                    res.writeHead(200, {'Content-Type': 'application/json'});
+                    res.end(JSON.stringify(row))
+                })
+                break
+            case "PUT":
+                var data = ''
+                req.on('data', (chunk) => {
+                    data += chunk
+                })
+                req.on('end', () => {
+                    app = JSON.parse(data)
+                    if (typeof app === 'object' && !Array.isArray(app) && app !== null && app.name !== undefined && app.version !== undefined && app.company !== undefined)
+                    {
+                        db.get('SELECT * FROM applications WHERE name = ?', [app.name], (err, row) => {
+                            if(err){
+                                console.log(err.message)
+                            }
+                            if(row != undefined && row.id != appNr)
+                            {
+                                res.writeHead(409, {'Content-Type': 'application/json'});
+                                res.end(`{"field" : "name", "message" : "duplicate"}`)
+                                return
+                            }
+                            db.run("DELETE FROM applications WHERE id=?", [appNr], function(err) {
+                                if(err){
+                                    console.log(err.message)
+                                    res.statusCode = 500
+                                    res.end()
+                                    return
+                                }
+                                if(this.changes == 0)
+                                {
+                                    res.statusCode = 404
+                                    res.end()
+                                    return
+                                }
+                                var query = `INSERT INTO applications (id, name, version, company
+                                    ${"app_hash" in app ? ', app_hash' : ''}
+                                    ${"includes_ads" in app ? ', includes_ads' : ''}
+                                    ${"downloads" in app ? ', downloads' : ''}) VALUES ( ?, ?, ?, ?
+                                    ${"app_hash" in app ? ', ?' : ''}
+                                    ${"includes_ads" in app ? ', ?' : ''}
+                                    ${"downloads" in app ? ', ?' : ''})`
+                                var queryArr = [appNr, app.name, app.version, app.company, app.app_hash, app.includes_ads, app.downloads]
+                                queryArr = queryArr.filter(function( element ) {
+                                    return element !== undefined;
+                                    });
+                                db.run(query, queryArr, (err) => {
+                                    if(err){
+                                        if(err.message.includes("UNIQUE"))
+                                        {
+                                            res.writeHead(409, {'Content-Type': 'application/json'});
+                                            res.end(`{"field" : "name", "message" : "duplicate"}`)
+                                            return
+                                        }
+                                        console.log(err.message)
+                                        res.statusCode = 500
+                                        res.end()
+                                        return
+                                    }
+                                    res.statusCode = 200
+                                    res.end()
+                                })
+                            })
+                        })
+                    }
+                    else{
+                        res.writeHead(400, {'Content-Type': 'text/html'});
+                        res.end(`wrong format`)
+                    }
+                })
+                break
+            default:
+                res.writeHead(405, {'Content-Type': 'text/html'})
+                res.end("Method not allowed")
         }
+    }
+    else{
+        res.statusCode = 404
+        res.end()
     }
 });
 
