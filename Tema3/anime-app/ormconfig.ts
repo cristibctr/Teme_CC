6c7caebd0d9bb16458c3026c6269aa9f3721241b
cristibctr <cristian.bucataru@gmail.com>
2022-03-29 08:37:09 +0300

    Tema3

---
+++ Tema3/anime-app/ormconfig.ts
@@ -0,0 +1,13 @@
+export default {
+  "type": "mysql",
+  "host": "34.148.232.77",
+  "port": 3306,
+  "username": "root",
+  "password": "",
+  "database": "anime-bd",
+  "synchronize": true,
+  "logging": true,
+  "entities": [
+    "entity/**/*.dto{.ts,.js}"
+  ]
+}
